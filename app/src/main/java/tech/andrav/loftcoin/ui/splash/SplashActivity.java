5c8f305b904e1c1f5585c1db3b0a8390968ae760
andrav24 <andrav24@yandex.ru>
2019-11-20 22:03:56 +0700

    Add Welcome screen

---
+++ app/src/main/java/tech/andrav/loftcoin/ui/splash/SplashActivity.java
@@ -0,0 +1,24 @@
+package tech.andrav.loftcoin.ui.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import tech.andrav.loftcoin.R;
+import tech.andrav.loftcoin.ui.welcome.WelcomeActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // после задержки запустить лямбду
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(this, WelcomeActivity.class));
+        }, 1000);
+    }
+}
---
+++ app/src/main/java/tech/andrav/loftcoin/ui/welcome/WelcomeActivity.java
@@ -0,0 +1,17 @@
+package tech.andrav.loftcoin.ui.welcome;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import tech.andrav.loftcoin.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
